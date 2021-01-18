6eccc2a9199ae92efec1a79405065dcbe47261c5
ibalpalaloi <ibalpalaloi@gmil.com>
2021-01-18 10:31:04 +0800

    modif route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/iqbal', function(){
+    dd('iqbal');
+});
+Route::get('/iqbal/delete', function(){
+    dd('delete');
+});
